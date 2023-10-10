7db7c907f73f6a63237ffa1c433da4d185f04e1b
jirigajdacz <jiri.gajdacz@email.cz>
2023-10-10 16:52:56 +0200

    init

---
+++ příklad.py
@@ -0,0 +1,9 @@
+books = [
+    {"title": "Zkus mě chytit", "sold": 4165, "price": 347, "year": 2018}, #0
+    {"title": "Vrah zavolá v deset", "sold": 5681, "price": 299, "year": 2019}, #1
+    {"title": "Zločinný steh", "sold": 2565, "price": 369, "year": 2019}, #2
+]
+
+print(books[2])
+print(books[0])
+print(books[1]["title"])
